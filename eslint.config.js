d138a1295037963aadf845cec52737d1156fe76c
Byonghun Kwak <byonghunkwak@gmail.com>
2026-01-21 20:22:26 -0800

    Update eslint.config.js file

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,10 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      "@typescript-eslint/no-explicit-any": "off",
+    },
   },
 ])
